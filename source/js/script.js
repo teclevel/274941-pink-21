e329689fb12a3379e1d1e1e321ac8d5673f11ada
Sergey <szaytsev060576@gmail.com>
2020-12-30 17:56:28 +0300

    main-nav

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,18 @@
+var pageHeader = document.querySelector('.page-header');
 var navMain = document.querySelector('.main-nav');
-      var navToggle = document.querySelector('.main-nav__toggle');
+var navToggle = document.querySelector('.main-nav__toggle');
 
-      navMain.classList.remove('main-nav--no-js');
 
-      navToggle.addEventListener('click', function() {
-        if (navMain.classList.contains('main-nav--closed')) {
-          navMain.classList.remove('main-nav--closed');
-          navMain.classList.add('main-nav--opened');
-        } else {
-          navMain.classList.add('main-nav--closed');
-          navMain.classList.remove('main-nav--opened');
-        }
-      });
+pageHeader.classList.remove('page-header--nojs');
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
