71f87f70783a7747c084b75b295bf609c609fac1
Sergey <szaytsev060576@gmail.com>
2021-01-07 17:46:16 +0300

    однотипность

--- source/js/script.js
+++ source/js/script.js
@@ -3,8 +3,8 @@ var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
 
-pageHeader.classList.remove('page-header--nojs');
-navMain.classList.remove('main-nav--nojs');
+pageHeader.classList.remove('page-header--no-js');
+navMain.classList.remove('main-nav--no-js');
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
